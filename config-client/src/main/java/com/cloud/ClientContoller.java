3f67451cab5ef00bf6fcb62501841092273f4a2e
jianmin qi <qijianmin@lexue.com>
2017-08-24 18:01:27 +0800

    config 分布式配置，即通过注册中心

--- config-client/src/main/java/com/cloud/ClientContoller.java
+++ config-client/src/main/java/com/cloud/ClientContoller.java
@@ -1,8 +1,11 @@
 package com.cloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by UI03 on 2017/8/23.
@@ -10,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientContoller {
 
+    Logger logger = LoggerFactory.getLogger(ClientContoller.class) ;
     @Value("${foo}")
     String foo ;
 
     @GetMapping("hi")
     public String hi(){
+        logger.info("hi");
         return "hi :"+ foo ;
     }
 }
